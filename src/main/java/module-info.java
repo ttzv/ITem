91b9a8081cf674a1b942900f61b1783993565771
ttzv <tomasz.zwak@gmail.com>
2019-12-08 19:29:20 +0100

    remove jdk.crypto.ec module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.ttzv.itmg {
+module com.ttzv.item {
     requires javafx.graphics;
     requires java.naming;
     requires javafx.controls;
@@ -9,14 +9,12 @@ module com.ttzv.itmg {
     requires jakarta.mail;
     requires org.update4j;
     requires ttzv.uiUtils;
-    requires ttzv.propsicl;
-    requires jdk.crypto.ec;//TODO: required for gmail's elliptic curve interface, remove this and bake into jlink image with system modules
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires ttzv.propsicl;
 
     opens com.ttzv.item to javafx.fxml;
     opens com.ttzv.item.ui.mainAppWindow to javafx.fxml;
-    opens com.ttzv.item.ui.mainAppWindow.popups to javafx.fxml;
     opens com.ttzv.item.ui.mailerWindow to javafx.fxml;
     opens com.ttzv.item.ui.placeholderWindow to javafx.fxml;
     opens com.ttzv.item.ui.settingsWindow to javafx.fxml;
