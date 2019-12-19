4ea0f5984b5a84ab0c70257ce8ce87eb5f47c1c2
ttzv <tomasz.zwak@gmail.com>
2019-12-19 19:56:18 +0100

    Implement entity that stores CommandItem entities to separate logic from ui

--- src/main/java/com/ttzv/item/entity/CommandBox.java
+++ src/main/java/com/ttzv/item/entity/CommandBox.java
@@ -3,25 +3,62 @@ package com.ttzv.item.entity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import javax.naming.NamingException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CommandBox {
 
+    private EntityDAO<CommandItem> commandItemEntityDAO;
     private List<CommandItem> commandItemList;
+    private List<Integer> commandItemUidList;
 
-    public CommandBox() {
+    public CommandBox(EntityDAO<CommandItem> commandItemEntityDAO) throws SQLException, NamingException, GeneralSecurityException, IOException {
+        if(commandItemEntityDAO != null) {
+            this.commandItemEntityDAO = commandItemEntityDAO;
+            this.commandItemList = commandItemEntityDAO.getAllEntities();
+            commandItemUidList = commandItemList.stream().map(c -> Integer.parseInt(c.getUid())).collect(Collectors.toList());
+        } else {
+            this.commandItemList = new ArrayList<>();
+            this.commandItemUidList = new ArrayList<>();
+        }
     }
 
-    public void add(CommandItem commandItem){
+    public List<CommandItem> getAll(){
+        return commandItemList;
+    }
+
+    public void reload() throws SQLException, NamingException, GeneralSecurityException, IOException {
+        this.commandItemList = commandItemEntityDAO.getAllEntities();
+        this.commandItemUidList = commandItemList.stream().map(c -> Integer.parseInt(c.getUid())).collect(Collectors.toList());
+    }
+
+    public Integer getNextUid(){
+        Collections.sort(commandItemUidList);
+        if(commandItemUidList != null && commandItemUidList.size() > 0) {
+            return commandItemUidList.get(commandItemUidList.size() - 1) + 1;
+        } else {
+            return null;
+        }
+    }
+
+    public void update(CommandItem commandItem) throws IOException, SQLException {
         commandItemList.add(commandItem);
+        commandItemUidList.add(Integer.parseInt(commandItem.getUid()));
+        commandItemEntityDAO.updateEntity(commandItem);
     }
 
-    public void remove (CommandItem commandItem){
+    public void remove(CommandItem commandItem) throws IOException, SQLException {
         commandItemList.remove(commandItem);
+        commandItemUidList.remove(Integer.valueOf(commandItem.getUid()));
+        commandItemEntityDAO.deleteEntity(commandItem);
     }
 
-    public ObservableList<CommandItem> getObservableList(){
-        return FXCollections.observableList(commandItemList);
-    }
+
 
 }
